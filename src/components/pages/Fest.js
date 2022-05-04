201b9d456d838f34f9823b4ff233d16f0f4b6431
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-05-04 11:48:47 +0200

    Versuch Fest Object zu id finden

--- src/components/pages/Fest.js
+++ src/components/pages/Fest.js
@@ -11,7 +11,27 @@ import Fester from '../Fester.json'
 export default function Fest(props) {
   let { festname } = useParams();
 
+  let users = {
+    'users': {
+      'user1': {
+        'id': '1',
+        'name': 'Brandon',
+        'DOB': '05/04/2000'
+      },
+      'user2': {
+        'id': '2',
+        'name': 'Jefferson',
+        'DOB': '05/19/2004'
+      }
+    }
+  }
   
+  const findUserById = (id) => {
+    const key = Object.keys(users.users).find(user => users.users[user].id === '1')
+    return users.users[key]
+  }
+  
+  console.log(findUserById('1'))
  
 
   return (
