78b95ce06a3038d5f00844f9feb8e010142a4c26
zech.emanuel <emanuel.zech@gmail.com>
2022-05-05 10:47:37 +0200

    fest output

--- src/components/pages/Fest.js
+++ src/components/pages/Fest.js
@@ -1,38 +1,36 @@
-import React from 'react'
-import '../../App.css'
-import'./Fest.css'
+import React from "react";
+import "../../App.css";
+import "./Fest.css";
 import { useParams, useNavigate } from "react-router-dom";
-import FestMainInfos from '../FestMainInfos'
-import FestOtherInfos from '../FestOtherInfos'
-import { useState, useEffect } from 'react';
+import FestMainInfos from "../FestMainInfos";
+import FestOtherInfos from "../FestOtherInfos";
+import { useState, useEffect } from "react";
 
-import Fester from '../Fester.json'
+import Fester from "../Fester.json";
 
 export default function Fest(props) {
   let { festname } = useParams();
 
-
-  
+  let data = Fester.filter((fest) => fest.name == festname);
 
   return (
+    (
       <>
-      <div className='fest-main'>
-        <div className='fest-display'>
-        <div className='header'>
-          { festname }
-        </div>
-          <div className='infos'>
-            <div className='infos-main'>
-              <FestMainInfos />
-            </div>
-            <div className='infos-other'>
-              <FestOtherInfos/>
+        <div className="fest-main">
+          <div className="fest-display">
+            <div className="header">{data[0].name}</div>
+            <div className="infos">
+              <div className="infos-main">
+                <div className="location">{data[0].fest[0].location}</div>
+                <div className="date">{data[0].fest[0].date}</div>
+              </div>
+              <div className="infos-other">
+                <FestOtherInfos />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      
       </>
-    
     )
+  );
 }
