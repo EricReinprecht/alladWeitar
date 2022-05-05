631677c3862677c389120e5dbf394b9a57779f48
zech.emanuel <emanuel.zech@gmail.com>
2022-05-05 10:52:25 +0200

    output data

--- src/components/pages/Fest.js
+++ src/components/pages/Fest.js
@@ -21,8 +21,10 @@ export default function Fest(props) {
             <div className="header">{data[0].name}</div>
             <div className="infos">
               <div className="infos-main">
-                <div className="location">{data[0].fest[0].location}</div>
-                <div className="date">{data[0].fest[0].date}</div>
+                <div className="info location">{data[0].fest[0].location}</div>
+                <div className="info date">{data[0].fest[0].date}</div>
+                <div className="info startTime">{data[0].fest[0].startTime}</div>
+                <div className="info otherInfos">{data[0].fest[0].otherInfos}</div>
               </div>
               <div className="infos-other">
                 <FestOtherInfos />
