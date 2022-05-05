ac4a92795675f91758050f9eb713425a3a953706
zech.emanuel <emanuel.zech@gmail.com>
2022-05-05 11:28:10 +0200

    output values

--- src/components/pages/Fest.js
+++ src/components/pages/Fest.js
@@ -13,26 +13,24 @@ export default function Fest(props) {
 
   let data = Fester.filter((fest) => fest.name == festname);
 
+
   return (
-    (
-      <>
-        <div className="fest-main">
-          <div className="fest-display">
-            <div className="header">{data[0].name}</div>
-            <div className="infos">
-              <div className="infos-main">
-                <div className="info location">{data[0].fest[0].location}</div>
-                <div className="info date">{data[0].fest[0].date}</div>
-                <div className="info startTime">{data[0].fest[0].startTime}</div>
-                <div className="info otherInfos">{data[0].fest[0].otherInfos}</div>
-              </div>
-              <div className="infos-other">
-                <FestOtherInfos />
-              </div>
+    <>
+      <div className="fest-main">
+        <div className="fest-display">
+          <div className="header">{data[0].name}</div>
+          <div className="infos">
+            <div className="infos-main">
+              <div className="info location">{data[0].fest[0].location}</div>
+              <div className="info date">{data[0].fest[0].date}</div>
+              <div className="info startTime">{data[0].fest[0].startTime}</div>
+            </div>
+            <div className="infos-other">
+              <div className="otherInfos"></div>
             </div>
           </div>
         </div>
-      </>
-    )
+      </div>
+    </>
   );
 }
