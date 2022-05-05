f7dbaf40e1de8891b42c2129b5fbc0f19f3c9e88
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-05-05 10:16:05 +0200

    Versuch gelöscht

--- src/components/pages/Fest.js
+++ src/components/pages/Fest.js
@@ -11,32 +11,13 @@ import Fester from '../Fester.json'
 export default function Fest(props) {
   let { festname } = useParams();
 
-  let users = {
-    'users': {
-      'user1': {
-        'id': '1',
-        'name': 'Brandon',
-        'DOB': '05/04/2000'
-      },
-      'user2': {
-        'id': '2',
-        'name': 'Jefferson',
-        'DOB': '05/19/2004'
-      }
-    }
-  }
+
   
-  const findUserById = (id) => {
-    const key = Object.keys(users.users).find(user => users.users[user].id === '1')
-    return users.users[key]
-  }
-  
-  console.log(findUserById('1'))
- 
 
   return (
       <>
-      <div className='test'>
+      <div className='fest-main'></div>
+      <div className='header'>
         { festname }
       </div>
       </>
