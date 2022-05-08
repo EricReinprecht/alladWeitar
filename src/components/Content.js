5d32aeeb57916cac66320ce24e61c3d93c59a6b8
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-05-08 13:11:04 +0200

    ufgrummt

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './Content.css'
 
 export default function Content(props) {
+  
   let navigate = useNavigate();
-  let { festname } = useParams();
   
   return (
     <>
