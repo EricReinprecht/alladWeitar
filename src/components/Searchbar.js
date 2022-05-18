f9ccd204df5e4911f1601442818ce72282496a73
zech.emanuel <emanuel.zech@gmail.com>
2022-05-18 11:23:16 +0200

    searchbar erstellt

---
+++ src/components/Searchbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Searchbar.css";
+
+export default function Searchbar() {
+  return (
+    <form action="">
+    <input type="search" placeholder="Search here ..."></input>
+    <i class="fa fa-search"></i>
+  </form>
+  );
+}
