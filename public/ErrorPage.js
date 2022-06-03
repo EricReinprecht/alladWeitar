d77bd0b5f14d34595dc9d08209744497566d5bb7
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-06-03 15:11:45 +0200

    ErrorPage update

---
+++ public/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ErrorPage.css'
+
+export default function ErrorPage() {
+  return (
+    <>
+    <div className='errorPage-main'>
+      <div className='error-massage'>Error ! Page not found</div>
+    </div>
+    </>
+  )
+}
