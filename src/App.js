0abd199e44e880c35a4fd78722af88c1f2b592d3
reinp <reinprechteric@gmail.com>
2023-08-16 14:15:48 +0200

    Update searchbar navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"
-import './App.css'
-import Home from './components/pages/Home'
-import FestPage from "./components/pages/FestPage";
-import ErrorPage from "./components/pages/ErrorPage";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/homepage/Home';
+import FestPage from './components/pages/FestPage';
+import ErrorPage from './components/pages/ErrorPage';
+import app from './firebaseConfig'; // Import the Firebase initialization file
+import Navbar from './components/NavBar/Navbar';
 
 export default function App() {
-
   return (
     <Router>
-    <Navbar />
+      <Navbar/>
       <Routes>
-          <Route path="/" element={<Home />} exact={true} />
-          <Route path="/fest/:festname" element={<FestPage />} exact={true} />
-          <Route path="/404" element={<ErrorPage />} exact={true}/>
+        <Route path="/" element={<Home />} exact={true} />
+        <Route path="/fest/:festname" element={<FestPage />} exact={true} />
+        <Route path="/404" element={<ErrorPage />} exact={true} />
       </Routes>
     </Router>
   );
